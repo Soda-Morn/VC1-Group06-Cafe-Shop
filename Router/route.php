12936091a2498bffc534714dec457ca32bdb20d2
Morn Soda <mornsoda23@gmail.com>
2025-03-09 14:03:00 +0700

    feat(feature/product_list):added branch into gitHub

--- Router/route.php
+++ Router/route.php
@@ -9,4 +9,12 @@ $route = new Router();
 //Dashboard Routs
 $route->get("/", [DashboardController::class, 'index']);
 
+// productList
+$routes->get('/user', [UserController::class, 'index']);
+$routes->get('/user/create', [UserController::class, 'create']);
+$routes->post('/user/store', [UserController::class, 'store']);
+$routes->get('/user/edit', [UserController::class, 'edit']);
+$routes->put('/user/update', [UserController::class, 'update']);
+$routes->delete('/user/delete', [UserController::class, 'destroy']);
+
 $route->route();
