d2fd3b5689a17c3a81c7d33cebb560c444defac4
unknown <rachana.chhum@student.passerellesnumeriques.org>
2025-03-09 15:55:35 +0700

    feal(feature/product_detail):added product_detial into branch

--- views/products/product_detail.php
+++ views/products/product_detail.php
@@ -1,55 +1,123 @@
 <div class="card">
-                  <div class="card-header">
-                    <div class="card-title">Responsive Table</div>
-                  </div>
-                  <div class="card-body">
-                    <div class="card-sub">
-                      <h1>Product Detail </h1>
-                    
-                    </div>
-                    <div class="table-responsive">
-                      <table class="table table-striped table-striped-bg-black">
-                        <thead>
-                          <tr>
-                            <th>ID</th>
-                            <th>PRODUCT IMAGE</th>
-                            <th>PRODUCT NAME</th>
-                            <th>QUANTITY</th>
-                            <th>PRICE</th>
-                            <th>ACTION</th>
-                           
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <th scope="row">1</th>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td><i class="fa-solid fa-star" style="color: #FFD43B;"></i></td>
-                            <td>Table cell</td>
-                       
-                          </tr>
-                          <tr>
-                            <th scope="row">2</th>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                         
-                          </tr>
-                          <tr>
-                            <th scope="row">3</th>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                        
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
+    <div class="card-header">
+        <div class="card-title">Responsive Table</div>
+    </div>
+    <div class="card-body">
+        <div class="card-sub">
+            <h1>Product Detail </h1>
+
+        </div>
+        <div class="table-responsive">
+            <table class="table table-striped table-striped-bg-black">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>PRODUCT IMAGE</th>
+                        <th>PRODUCT NAME</th>
+                        <th>QUANTITY</th>
+                        <th>PRICE</th>
+                        <th>ACTION</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr>
+                        <th scope="row">1</th>
+                        <td><i class="fa-regular fa-image"></i></td>
+                        <td>Coffee</td>
+
+
+                        <td><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"><i class="fa-solid fa-star" style="color: #FFD43B;"></i></i></td>
+                        <td>5.0$</td>
+                        <td><i class="fa-regular fa-pen-to-square m-3"></i><i class="fa-regular fa-trash-can" style="color: #ec1818;"></i></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td><i class="fa-regular fa-image"></i></td>
+                        <td>Coffee</td>
+
+
+                        <td><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"><i class="fa-solid fa-star" style="color: #FFD43B;"></i></i></td>
+                        <td>5.0$</td>
+                        <td><i class="fa-regular fa-pen-to-square m-3"></i><i class="fa-regular fa-trash-can" style="color: #ec1818;"></i></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td><i class="fa-regular fa-image"></i></td>
+                        <td>Coffee</td>
+
+
+                        <td><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"><i class="fa-solid fa-star" style="color: #FFD43B;"></i></i></td>
+                        <td>5.0$</td>
+                        <td><i class="fa-regular fa-pen-to-square m-3"></i><i class="fa-regular fa-trash-can" style="color: #ec1818;"></i></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td><i class="fa-regular fa-image"></i></td>
+                        <td>Coffee</td>
+
+
+                        <td><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"><i class="fa-solid fa-star" style="color: #FFD43B;"></i></i></td>
+                        <td>5.0$</td>
+                        <td><i class="fa-regular fa-pen-to-square m-3"></i><i class="fa-regular fa-trash-can" style="color: #ec1818;"></i></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td><i class="fa-regular fa-image"></i></td>
+                        <td>Coffee</td>
+
+
+                        <td><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"><i class="fa-solid fa-star" style="color: #FFD43B;"></i></i></td>
+                        <td>5.0$</td>
+                        <td><i class="fa-regular fa-pen-to-square m-3"></i><i class="fa-regular fa-trash-can" style="color: #ec1818;"></i></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">6</th>
+                        <td><i class="fa-regular fa-image"></i></td>
+                        <td>Coffee</td>
+
+
+                        <td><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"><i class="fa-solid fa-star" style="color: #FFD43B;"></i></i></td>
+                        <td>5.0$</td>
+                        <td><i class="fa-regular fa-pen-to-square m-3"></i><i class="fa-regular fa-trash-can" style="color: #ec1818;"></i></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">7</th>
+                        <td><i class="fa-regular fa-image"></i></td>
+                        <td>Coffee</td>
+
+
+                        <td><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"><i class="fa-solid fa-star" style="color: #FFD43B;"></i></i></td>
+                        <td>5.0$</td>
+                        <td><i class="fa-regular fa-pen-to-square m-3"></i><i class="fa-regular fa-trash-can" style="color: #ec1818;"></i></td>
+
+                    </tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
