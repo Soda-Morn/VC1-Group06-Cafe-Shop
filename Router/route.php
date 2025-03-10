0d567800d7d83a8ddd44615bd41df412e269f22a
unknown <Sokchea.Boy@pnc.lan>
2025-03-10 15:35:01 +0700

    feal(add stuck List): added interface[VC01G6-124]

--- Router/route.php
+++ Router/route.php
@@ -3,10 +3,13 @@ require_once "Router.php";
 require_once "Controllers/BaseController.php";
 require_once "Database/Database.php";
 require_once "Controllers/DashboardController.php";
+require_once "Controllers/StocklistController.php";
 
 
 $route = new Router();
 //Dashboard Routs
 $route->get("/", [DashboardController::class, 'index']);
+//Stocklist Routs
+$route->get("/stocklist", [StocklistController::class, 'stocklist']);
 
 $route->route();
--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -2,7 +2,7 @@
         <div class="sidebar-logo">
           <!-- Logo Header -->
           <div class="logo-header" data-background-color="dark">
-            <a href="index.html" class="logo">
+            <a href="/" class="logo">
               <img
                 src="views/assets/img/kaiadmin/logo_light.svg"
                 alt="navbar brand"
@@ -29,8 +29,7 @@
             <ul class="nav nav-secondary">
               <li class="nav-item active">
                 <a
-                  data-bs-toggle="collapse"
-                  href="#dashboard"
+                  href="/"
                   class="collapsed"
                   aria-expanded="false"
                 >
@@ -125,7 +124,7 @@
                 <div class="collapse" id="tables">
                   <ul class="nav nav-collapse">
                     <li>
-                      <a href="tables/tables.html">
+                      <a href="/stocklist">
                         <span class="sub-item">Stock List</span>
                       </a>
                     </li>
