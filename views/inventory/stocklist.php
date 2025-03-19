912446ef989f9ea2276f7ce5638bd1443ee0c28e
sokchea-boy <sokchea.boy@student.passerellesnumeriques.org>
2025-03-19 12:18:50 +0700

    feat(stocklist):added stocklist ot database[VC01G6-226]

--- views/inventory/stocklist.php
+++ views/inventory/stocklist.php
@@ -1,12 +1,12 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-19">
             <div class="card">
                 <div class="card-header">
                     <h4 class="card-title ">Stock Inventory List</h4>
                 </div>
                 <div class="card-body">
-                    <div class="table-responsive">
+                    <div class="table-responsive ">
                         <table id="basic-datatables" class="display table table-striped table-hover ">
                             <thead >
                                 <tr >
@@ -15,267 +15,39 @@
                                     <th class = "bg-warning">PRODUCTS</th>
                                     <th class = "bg-warning">DATE ADDED</th>
                                     <th class = "bg-warning">STOCK</th>
-                                    <th class = "bg-warning">STATUS</th>
+                                    <th class = "bg-warning ">STATUS</th>
+                                    <th class = "bg-warning ">option</th>
                                 </tr>
                             </thead>
-                            <tfoot>
-                          <tr>
-                            <th>ID</th>
-                            <th>Images</th>
-                            <th>PRODUCTS</th>
-                            <th>CATEGORY</th>
-                            <th>DATE ADDED</th>
-                            <th>STOCK</th>
-                            <th>STARTUS</th>
-                          </tr>
-                        </tfoot>
+
+                        
                             <tbody>
-                                <tr>
-                                    <td>#1</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 1.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td ><span class= " bg-success mr-3 p-2 rounded-4 ">Out of Stock</span></td>
-                                </tr>
+                               
+                        <?php foreach ($stocklist as $row) : ?>
+                             <?php
+                             $statusClass = ($row['Stock'] > 0) ? "bg-success" : "bg-danger";
+                             $statusText = ($row['Stock'] > 0) ? "In Stock" : "Out of Stock";
+                             ?>
+                             <tr>
+                                 <td><?=$row['ID']; ?></td>
+                                 <td>
+                                     <img src="<?=$row['Image']; ?>" alt="<?= $row['Products']; ?>" style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;">
+                                 </td>
+                                 <td><?= $row['Products']; ?></td>
+                                 <td><?= date('F j, Y', strtotime($row['DATE ADDED'])); ?></td>
 
-                                <tr>
-                                    <td>#2</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">in of stock</span></td>
-                                </tr>
+                                 <td><?= $row['ProductName']; ?></td>
+                                 <td>
+                                     <span class="<?=$statusClass; ?><?= $row['Stock']; ?> mr-9 p-2 rounded-4"><?= $statusText; ?></span>
+                                 </td>
+                                 <!-- <td><?= $row['Stock']; ?></td> -->
+                                 <td><a href=""><i class="fas fa-edit me-2"></i></a></td>
 
-                                <tr>
-                                    <td>#3</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 1.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-success mr-3 p-2 rounded-4 ">Out of Stock</span></td>
-                                </tr>
-
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 1.jpg"
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-success mr-3 p-2 rounded-4 ">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 1.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-success mr-3 p-2 rounded-4 ">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                                <tr>
-                                    <td>#4</td>
-                                    <td><img src="/views/assets/img/examples/examplecoffee 2.jpg "
-                                             alt="System Architect "  
-                                             style="width: 38px; height: 38px; border-radius: 50%; object-fit: cover;"> </td>
-                                    <td>Cappuccino</td>
-                                    <td>coffee</td>
-                                    <td>june 17,2025</td>
-                                    <td>100</td>
-                                    <td><span class= "bg-warning mr-3 p-2 rounded-4">Out of Stock</span></td>
-                                </tr>
-                            </tbody>
+                                 
+                             </tr>
+                             
+                             <?php endforeach; ?>
+                                </tbody>
                         </table>
                     </div>
                 </div>
